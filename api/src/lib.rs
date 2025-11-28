1c989d9c1cbb7fc4addd1e6b2a7f36d132a8f55b
SforSam <81501696+ItsSforSam@users.noreply.github.com>
2025-11-27 23:15:45 -0600

    Fixed syscall bug

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -1,36 +1,69 @@
 #![no_std]
-// #![feature(c_variadic)]
+#![feature(c_variadic)]
+use core::ffi::va_list::VaList;
 use api_sys as sys;
 use core::ffi::{self, c_double, c_long};
+
+
 #[macro_use]
 pub mod macros;
-
-#[cfg(target_arch="x86_64")]
-#[path ="arch/x86-64.rs"]
 pub mod arch;
-#[path ="arch/common.rs"]
-pub mod arch_common;
-// SAFETY
+
+
+
+// # Safety
 // Calling 
-pub unsafe extern "C" fn syscall(call:c_double,mut args:VaListImpl<'_>) -> c_long{
+pub unsafe extern "C" fn syscall(_call:c_double,mut args:...) -> c_long{
+    // @TODO make list
     let a:c_long;
     let b:c_long;
     let c:c_long;
     let d:c_long;
     let e:c_long;
     let f:c_long;
+
+    // Safety: We may just clobber
     unsafe{
         a=args.arg();
         b=args.arg();
+        c=args.arg();
         d=args.arg();
         e=args.arg();
         f=args.arg();
+
+        crate::arch::current::syscall6(a, b, c, d, e, f, g)
     }
-    todo!()
+    
 }
 
 
-pub fn write(){
+pub fn mmap(
+    addr:*mut ffi::c_void,
+    size: usize, // size_t
+    proto:ffi::c_int,
+    flags: ffi::c_int,
+    fd: ffi::c_int,
+    offset:c_long
+)->*mut ffi::c_void{
+    const MMAP2:core::ffi::c_long = 192; // For some reason the sys crate isn't including
+    // SYS_mmap2, but this is what the defined value is
+    unsafe {syscall!(MMAP2,
+                    addr as _,
+                    size as _,
+                    proto as _,
+                    flags as _,
+                    fd as _,
+                    offset as _
+                ) as _}
+}
 
+
+extern "C" fn write(
+    fd:ffi::c_int,
+    buffer:*const ffi::c_void,
+    size: usize
+
+)->isize{ // ssize_t
+    todo!()
 }
 
