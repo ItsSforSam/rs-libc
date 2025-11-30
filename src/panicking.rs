58e0a7adfd311111e9c3cbf1c3ab6e68031a330b
SforSam <81501696+ItsSforSam@users.noreply.github.com>
2025-11-30 16:37:16 -0600

    Add eh_personally lang and oom handler
    Both to be implimented
    
    the eh_personally is mostly implimented to stop ld complaining about undefined symbol

--- src/panicking.rs
+++ src/panicking.rs
@@ -12,3 +12,19 @@ pub extern "Rust" fn __panic_impl(_i:&PanicInfo)->!{
 fn panic_handler(info:&PanicInfo) ->!{
     __panic_impl(info)
 }
+// https://github.com/rust-lang/rust/blob/HEAD/library/std/src/sys/personality/gcc.rs
+// If Rust supplies this, use it, but mostly us used to prevent ld to stop complaining
+// that "rust_eh_personality" is not defined
+#[lang = "eh_personality"]
+#[linkage = "weak"]
+#[cfg(not(test))]
+#[doc(hidden)]
+pub extern "C" fn rust_eh_personality() {}
+
+// Safety: has special prefix
+#[unsafe(no_mangle)]
+#[alloc_error_handler]
+pub fn __rslibc_oom_error(layout: core::alloc::Layout)->!{
+    todo!()
+
+}
