94903f370aa0f60d35461a60562090199d7f3b9a
SforSam <81501696+ItsSforSam@users.noreply.github.com>
2025-11-30 16:19:05 -0600

    Has global allocator set

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
 #![no_std]
 #![no_main]
-#![feature(linkage)]
+#![allow(internal_features, reason ="Uses lang item of eh_personality")]
+#![feature(linkage,lang_items,alloc_error_handler)]
 mod panicking;
 mod rt;
 pub mod env;
 extern crate alloc;
 
+#[global_allocator]
+static ALLOCATOR: memory::Allocator = memory::Allocator::new();
