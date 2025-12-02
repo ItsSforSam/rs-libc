fc681bb395884216e37aa849a3bbfa0a56b24f48
SforSam <81501696+ItsSforSam@users.noreply.github.com>
2025-12-02 14:49:45 -0600

    Added docs to panicking

--- src/panicking.rs
+++ src/panicking.rs
@@ -1,7 +1,12 @@
-
+//! Panicking runtime
+//! 
+//! The exact behavior of a panic should not be relied upon.
+//! 
+//! When a panic occurs it should be treated as a unresolvable error.
+//! A backtrace may or may not be given.
+//! 
 use core::panic::PanicInfo;
 
-#[linkage = "weak"]
 #[unsafe(no_mangle)]
 pub extern "Rust" fn __panic_impl(_i:&PanicInfo)->!{
     todo!()
@@ -24,7 +29,7 @@ pub extern "C" fn rust_eh_personality() {}
 // Safety: has special prefix
 #[unsafe(no_mangle)]
 #[alloc_error_handler]
-pub fn __rslibc_oom_error(layout: core::alloc::Layout)->!{
+pub fn __rslibc_oom_error(_: core::alloc::Layout)->!{
     todo!()
 
 }
