d447a6bc302590b2a7ed98e3dcfe7168234daaeb
SforSam <81501696+ItsSforSam@users.noreply.github.com>
2025-12-02 14:49:23 -0600

    Create a basemark for environment

--- src/env.rs
+++ src/env.rs
@@ -2,19 +2,33 @@
 //! 
 //! An
 
-use core::ffi::CStr;
+use core::ffi::{CStr, c_char};
 
 /// This allows lazily be placed in 
 #[derive(Debug)]
-struct EnvVars{
+pub struct EnvVars{
 
-    // data:alloc::collections::Mutex<alloc::vec::Vec<CStr>>
+    
 }
 
+#[expect(clippy::new_without_default)]
 impl EnvVars{
     pub const fn new()->Self{
         EnvVars {
-            // data: Vec::new()
+            
         }
     }
+
+    unsafe fn from_ptr(ptr:*mut *mut c_char) ->Self{
+        todo!()
+    }
+
+    
 }
+
+
+// SAFETY: Non
+#[unsafe(export_name = "__environ")]
+#[linkage = "weak"]
+// #[alias = "_environ"]
+pub(crate) static mut RAW_ENVIRON: *mut *mut c_char = core::ptr::null_mut();
