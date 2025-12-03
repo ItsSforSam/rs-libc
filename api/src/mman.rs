5d691a264078200c120a747fef3433844ced0ad2
SforSam <81501696+ItsSforSam@users.noreply.github.com>
2025-12-03 14:15:38 -0600

    Fixed munmap
    I implemented it wrong, now fixed

--- api/src/mman.rs
+++ api/src/mman.rs
@@ -2,7 +2,7 @@ use crate::prelude::*;
 
 pub fn munmap(addr: *mut c_void,len:usize)->Result<(),crate::errno::Errno>{
     
-    if unsafe {syscall!(api_sys::SYS_munmap)} == -1{
+    if unsafe {syscall!(api_sys::SYS_munmap, addr, len)} == -1{
         Errno::try_from(ERRNO.get() as u32).unwrap();
     }
     Ok(())
