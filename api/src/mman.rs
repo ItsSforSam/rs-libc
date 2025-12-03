0feb704341eb659bb085166227a1070ab2bd8a87
SforSam <81501696+ItsSforSam@users.noreply.github.com>
2025-12-03 14:14:40 -0600

    Implement munmap

--- api/src/mman.rs
+++ api/src/mman.rs
@@ -1,8 +1,8 @@
 use crate::prelude::*;
 
 pub fn munmap(addr: *mut c_void,len:usize)->Result<(),crate::errno::Errno>{
-    todo!();
-    if unsafe {syscall!(sys::SYS_munmap)} == -1{
+    
+    if unsafe {syscall!(api_sys::SYS_munmap)} == -1{
         Errno::try_from(ERRNO.get() as u32).unwrap();
     }
     Ok(())
