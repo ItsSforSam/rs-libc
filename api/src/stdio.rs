42de4467533d298005ec4a9501ec7886718fae8e
SforSam <81501696+ItsSforSam@users.noreply.github.com>
2025-12-05 14:51:55 -0600

    Start stdio

--- api/src/stdio.rs
+++ api/src/stdio.rs
@@ -1,5 +1,24 @@
+use core::usize;
 
-/// 
+use crate::passing;
+
+
+// This is passed out thru the api
+// 
+// This should be passed as a pointer outwards as `*mut File`
+/// A Representation of a open file
+#[non_exhaustive]
 pub struct File{
-
+    fd:usize
+}
+impl File{
+    pub const fn new(fd:usize)->Self{
+        File { fd }
+    }
+    
+}
+unsafe impl passing::PassOff for File{
+     fn pass_off(self) -> *mut File{
+        
+    }
 }
