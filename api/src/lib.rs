4ed28240293eceaccc30c9daf10887899beb69f8
SforSam <81501696+ItsSforSam@users.noreply.github.com>
2025-12-11 14:38:04 -0600

    Moves mmap and related flags to mman

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -1,13 +1,14 @@
+//! The api of the crate
+//! 
+//! These allow calling syscalls to the system (currently only Linux)
 #![no_std]
 #![feature(c_variadic)]
 #![feature(thread_local)]
 #![expect(internal_features, reason ="Uses abort intrinsic, which is safe")]
 #![feature(core_intrinsics)]
-use core::{ffi::va_list::VaList, mem};
-use api_sys::__pid_t;
-pub use api_sys as sys;
-use core::ffi::*;
-use bitflags::bitflags;
+
+extern crate api_sys as sys;
+// use bitflags::bitflags;
 
 #[macro_use]
 pub mod macros;
@@ -16,128 +17,84 @@ pub mod errno;
 pub mod stdio;
 pub mod mman;
 
+/// Most syscalls can fail with a errno value
+/// 
+/// This provides an alias that can be used when interacting with this crate
+pub type Result<T> = core::result::Result<T, crate::errno::Errno>;
+/// Equivalent to C's `size_t` type, from `stddef.h`.
+/// 
+/// Equivalent of Rust's [`core::ffi::c_size_t`] but not nightly
+#[expect(non_camel_case_types, reason = "Mimics rust type alias")]
+pub type c_ssize_t = isize;
+
+/// Equivalent to C's `size_t` type, from `stddef.h`.
+/// 
+/// Equivalent of Rust's [`core::ffi::c_ssize_t`] but not nightly
+#[expect(non_camel_case_types, reason = "Mimics rust type alias")]
+pub type c_size_t = usize;
 pub mod prelude{
     /// Due to the amount of ffi to C, it should be convenient.
     /// All ffi types are prefixed anyway
-    pub use core::ffi::*;
+    pub use core::ffi::{
+        c_char,c_double,c_uint,c_int,c_ulong,c_long,c_void
+    };
+    pub use crate::{c_size_t,c_ssize_t};
     pub use crate::errno::{Errno,ERRNO};
-    pub use crate::syscall;
 }
-
+use prelude::*;
 // # Safety
 // Calling 
-pub unsafe extern "C" fn syscall(_call:c_double,mut args:...) -> c_long{
-    // @TODO make list
-    let a:c_long;
-    let b:c_long;
-    let c:c_long;
-    let d:c_long;
-    let e:c_long;
-    let f:c_long;
-    let g:c_long;
-    // Safety: We may just clobber
-    unsafe{
-        a=args.arg();
-        b=args.arg();
-        c=args.arg();
-        d=args.arg();
-        e=args.arg();
-        f=args.arg();
-        g=args.arg();
-        crate::arch::current::syscall6(a, b, c, d, e, f, g)
-    }
+// pub unsafe extern "C" fn syscall(_call:c_long,mut args:...) -> c_long{
+//     // @TODO make list
+//     let a:c_long;
+//     let b:c_long;
+//     let c:c_long;
+//     let d:c_long;
+//     let e:c_long;
+//     let f:c_long;
+//     let g:c_long;
+//     // Safety: We may just clobber
+//     unsafe{
+//         a=args.arg();
+//         b=args.arg();
+//         c=args.arg();
+//         d=args.arg();
+//         e=args.arg();
+//         f=args.arg();
+//         g=args.arg();
+//         crate::arch::current::syscall6(a, b, c, d, e, f, g)
+//     }
     
-}
+// }
 
-bitflags! {
-    pub struct MMapProt: u32{
-        const Exec  = sys::PROT_EXEC;
-        const Read  = sys::PROT_READ;
-        const Write = sys::PROT_WRITE;
-        const None  = sys::PROT_NONE;
-    }
-}
 
-bitflags! {
-    pub struct MMapFlags: u32{
-        const Shared         = sys::MAP_SHARED;
-        const SharedValidate = sys::MAP_SHARED_VALIDATE;
-        const Private        = sys::MAP_PRIVATE;
-        const Bit32          = sys::MAP_32BIT;
-        const Anonymous      = sys::MAP_ANONYMOUS;
-        // Ignored as caused DDoS
-        const DenyWrite      = sys::MAP_DENYWRITE;
-        // Ignored
-        const Executable     = sys::MAP_EXECUTABLE;
 
-        const Fixed          = sys::MAP_FIXED;
-        const FixedNoReplace = sys::MAP_FIXED_NOREPLACE;
-        const GrowsDown      = sys::MAP_GROWSDOWN;
-        const HugetLB        = sys::MAP_HUGETLB;
-        const HugetLB_2MP    = sys::MAP_HUGE_2MB;
-        const HugetLB_1GB    = sys::MAP_HUGE_1GB;
 
-        const Locked         = sys::MAP_LOCKED;
-        const NonBlock       = sys::MAP_NONBLOCK;
-        const NoReserve      = sys::MAP_NORESERVE;
-        const Stack          = sys::MAP_STACK;
-        const Sync           = sys::MAP_SYNC;
-        // const Uninitialized =   sys::MAP_UNINITIALIZED;
-    }
-}
-
-pub fn mmap(
-    addr:*mut c_void,
-    size: usize, // size_t
-    proto:MMapProt,
-    flags: MMapFlags,
-    fd: c_int,
-    offset:c_long
-)->*mut c_void{
-    
-    const MMAP2:core::ffi::c_long = 192; // For some reason the sys crate isn't including
-    // SYS_mmap2, but this is what the defined value is
-    unsafe {syscall!(MMAP2,
-                    addr,
-                    size,
-                    proto.bits(),
-                    flags.bits(),
-                    fd,
-                    offset
-                ) as _}
-}
-
-#[unsafe(no_mangle)]
-pub extern "C" fn write(
-    fd:c_int,
-    buffer:*const c_void,
-    size: usize
-
-)->isize{ // ssize_t
-    unsafe{syscall!(sys::SYS_write,
-        fd,
-        buffer,
-         size) as _
-        } 
-}
 
 /// Aborts by calling signal
 /// 
-/// Currently if it fails it calls an invalid instruction via rust's
-/// []
+/// Currently if it fails it calls an invalid instruction via rust's [core::intrinsics::abort]
 #[expect(unreachable_code, reason = "We are trying to kill this program no matter what. So we try multiple never functions, even if they siminly never return")]
 pub fn abort()->!{
     // @TODO: unmask signal 
     kill(getpid(), api_sys::SIGABRT as _);
 
     core::intrinsics::abort();
-    // SAFETY: Technically this is UB, but we are intentionally accessing an invalid address
+    // SAFETY: Technically this is UB, but we are intentionally accessing an invalid address. But most
+    // OSes will respond with Seg fault. It technically is possible via mmap to have valid data there
+    // (but if you do that, your shooting yourself in the foot anyway) ¯\_(ツ)_/¯
     let _:u8 = unsafe {core::ptr::read_volatile(core::ptr::null())};
-    unreachable!("Abort function failed. Panicking");
+    
+    unreachable!("Abort function failed. No coredump generated");
 }
-fn getpid() -> api_sys::__pid_t{
-    unsafe {syscall!(api_sys::SYS_getpid) as _}
+pub fn getpid() -> i32{
+    // SAFETY: Provides required params (none)
+    // SAFETY: getpid does not error
+    unsafe {syscall!(api_sys::SYS_getpid).unwrap_unchecked() as _}
 }
-fn kill(pid:api_sys::__pid_t, sig:c_int)->c_int{
-   unsafe { syscall!(api_sys::SYS_kill,pid,sig) as _}
+pub fn kill(pid:i32, sig:c_int)->Result<c_int>{
+
+    // SAFETY: passes correct prams 
+   let v =unsafe { syscall!(api_sys::SYS_kill,pid,sig)}?;
+   Ok(v as c_int)
 }
--- api/src/mman.rs
+++ api/src/mman.rs
@@ -1,9 +1,73 @@
 use crate::prelude::*;
-
+use api_sys as sys;
+use bitflags::bitflags;
 pub fn munmap(addr: *mut c_void,len:usize)->Result<(),crate::errno::Errno>{
-    
-    if unsafe {syscall!(api_sys::SYS_munmap, addr, len)} == -1{
-        Errno::try_from(ERRNO.get() as u32).unwrap();
-    }
+    //SAFETY: calls with proper parameters of correct type
+    unsafe {crate::syscall!(sys::SYS_munmap, addr, len)}?;
     Ok(())
+}
+
+bitflags! {
+    pub struct MMapProt: c_uint{
+        const Exec  = sys::PROT_EXEC;
+        const Read  = sys::PROT_READ;
+        const Write = sys::PROT_WRITE;
+        const None  = sys::PROT_NONE;
+    }
+}
+
+bitflags! {
+    pub struct MMapFlags: u32{
+        const Shared         = sys::MAP_SHARED;
+        const SharedValidate = sys::MAP_SHARED_VALIDATE;
+        const Private        = sys::MAP_PRIVATE;
+        const Bit32          = sys::MAP_32BIT;
+        const Anonymous      = sys::MAP_ANONYMOUS;
+        // Ignored as caused DDoS
+        const DenyWrite      = sys::MAP_DENYWRITE;
+        // Ignored
+        const Executable     = sys::MAP_EXECUTABLE;
+
+        const Fixed          = sys::MAP_FIXED;
+        const FixedNoReplace = sys::MAP_FIXED_NOREPLACE;
+        const GrowsDown      = sys::MAP_GROWSDOWN;
+        const HugetLB        = sys::MAP_HUGETLB;
+        // const HugetLB_2MP    = sys::MAP_HUGE_2MB;
+        // const HugetLB_1GB    = sys::MAP_HUGE_1GB;
+
+        const Locked         = sys::MAP_LOCKED;
+        const NonBlock       = sys::MAP_NONBLOCK;
+        const NoReserve      = sys::MAP_NORESERVE;
+        const Stack          = sys::MAP_STACK;
+        const Sync           = sys::MAP_SYNC;
+        // const Uninitialized =   sys::MAP_UNINITIALIZED;
+        // Have all bytes be "known" as more flags can be introduced
+        //
+        const _ = !0;
+    }
+}
+
+
+pub fn mmap(
+    addr:*mut c_void,
+    size: usize, // size_t
+    proto:MMapProt,
+    flags: MMapFlags,
+    fd: c_int,
+    offset:c_long
+)->crate::Result<*mut c_void>{
+    
+    const MMAP2:core::ffi::c_long = 192; // For some reason the sys crate isn't including
+    // SYS_mmap2, but this is what the defined value is
+    //SAFETY: Puts the proper syscall with the proper parameters
+    let v = unsafe {crate::syscall!(MMAP2,
+                    addr,
+                    size,
+                    proto.bits(),
+                    flags.bits(),
+                    fd,
+                    offset
+                )}?;
+    Ok(v as *mut c_void)
+
 }
