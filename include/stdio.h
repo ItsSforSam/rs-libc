45f5b5300f53aeb064359e06dd8dff474408c10e
SforSam <81501696+ItsSforSam@users.noreply.github.com>
2025-12-11 14:42:32 -0600

    Have stdio.h to properly be bootstrapped

--- include/stdio.h
+++ include/stdio.h
@@ -3,6 +3,12 @@
 
 
 #include <stddef.h>
+#ifdef __RSLIBC_BUILD_BOOTSTRAP
 extern size_t write(int fd,const void* buffer, size_t nbytes);
+
+/* Opaque struct */
+typedef struct FILE;
+
+#endif
 
 #endif
