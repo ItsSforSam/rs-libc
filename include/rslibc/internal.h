bf0f06e9692ccc06d4faceed8db1f78975c4c401
SforSam <81501696+ItsSforSam@users.noreply.github.com>
2025-12-12 14:33:42 -0600

    Cleans up internal header

--- include/rslibc/internal.h
+++ include/rslibc/internal.h
@@ -5,12 +5,6 @@
 #ifndef __rslibc_misc_internal
 #define __rslibc_misc_internal
 
-#ifdef __GNUC__
 
-/* Define some macros used by GCC's headers like there bits/syscall.h errors if included explicitly and not by there sys/syscall.h*/
-/* @TODO: Not to rely on GCC's headers*/
-#define _SYSCALL_H 1
-
-#endif
 
 #endif /* __rslibc_misc_internal include guard*/
