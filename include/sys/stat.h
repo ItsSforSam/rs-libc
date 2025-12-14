5a165dba86ffb85db2328acd9c7013507ae77ece
SforSam <81501696+ItsSforSam@users.noreply.github.com>
2025-12-14 14:00:47 -0600

    Update stat.h

--- include/sys/stat.h
+++ include/sys/stat.h
@@ -8,31 +8,31 @@ https://github.com/torvalds/linux/blob/d358e5254674b70f34c847715ca509e46eb81e6f/
 #define _SYS_STAT_H
 #include <asm-generic/fcntl.h>
 #ifndef O_APPEND
-#define O_APPEND	0x0008
+    #define O_APPEND	0x0008
 #endif
 #ifndef O_DSYNC
-#define O_DSYNC		0x0010	/* used to be O_SYNC, see below */
+    #define O_DSYNC		0x0010	/* used to be O_SYNC, see below */
 #endif
 #ifndef O_NONBLOCK
-#define O_NONBLOCK	0x0080
+    #define O_NONBLOCK	0x0080
 #endif
-#ifndef O_CREAT
+    #ifndef O_CREAT
 #define O_CREAT		0x0100	/* not fcntl */
 #endif
 #ifdef O_TRUNC
-#define O_TRUNC		0x0200	/* not fcntl */
+    #define O_TRUNC		0x0200	/* not fcntl */
 #endif
 #ifndef O_EXCL
-#define O_EXCL		0x0400	/* not fcntl */
+    #define O_EXCL		0x0400	/* not fcntl */
 #endif
 #ifndef O_NOCTTY
-#define O_NOCTTY	0x0800	/* not fcntl */
+    #define O_NOCTTY	0x0800	/* not fcntl */
 #endif
-#ifndef FASYNC
+    #ifndef FASYNC
 #define FASYNC		0x1000	/* fcntl, for BSD compatibility */
 #endif
 #ifndef O_LARGEFILE
-#define O_LARGEFILE	0x2000	/* allow large file opens */
+    #define O_LARGEFILE	0x2000	/* allow large file opens */
 #endif
 /*
  * Before Linux 2.6.33 only O_DSYNC semantics were implemented, but using
@@ -48,11 +48,11 @@ https://github.com/torvalds/linux/blob/d358e5254674b70f34c847715ca509e46eb81e6f/
  * Note: __O_SYNC must never be used directly.
  */
 #ifndef O_SYNC
-#define __O_SYNC	0x4000
-#define O_SYNC		(__O_SYNC|O_DSYNC)
+    #define __O_SYNC	0x4000
+    #define O_SYNC		(__O_SYNC|O_DSYNC)
 #endif /*O_SYNC*/
 #ifndef O_DIRECT
-#define O_DIRECT	0x8000	/* direct disk access hint */
+    #define O_DIRECT	0x8000	/* direct disk access hint */
 #endif
 #ifndef F_GETLK
 #define F_GETLK		14
