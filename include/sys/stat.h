6bac39f52484bc7169799f7138030736a6b5f6a7
SforSam <81501696+ItsSforSam@users.noreply.github.com>
2025-12-14 12:35:27 -0600

    Update include/sys/stat.h

--- include/sys/stat.h
+++ include/sys/stat.h
@@ -4,19 +4,36 @@ This ports linux headers which is under GPL-2.0 WITH linux-syscall-note
 https://github.com/torvalds/linux/blob/d358e5254674b70f34c847715ca509e46eb81e6f/arch/mips/include/uapi/asm/fcntl.h#L17
 */
 
-#ifndef _SYS_MMAN_H
-#define _SYS_MMAN_H
+#ifndef _SYS_STAT_H
+#define _SYS_STAT_H
 #include <asm-generic/fcntl.h>
-// #define O_APPEND	0x0008
-// #define O_DSYNC		0x0010	/* used to be O_SYNC, see below */
-// #define O_NONBLOCK	0x0080
-// #define O_CREAT		0x0100	/* not fcntl */
-// #define O_TRUNC		0x0200	/* not fcntl */
-// #define O_EXCL		0x0400	/* not fcntl */
-// #define O_NOCTTY	0x0800	/* not fcntl */
-// #define FASYNC		0x1000	/* fcntl, for BSD compatibility */
-// #define O_LARGEFILE	0x2000	/* allow large file opens */
-
+#ifndef O_APPEND
+#define O_APPEND	0x0008
+#endif
+#ifndef O_DSYNC
+#define O_DSYNC		0x0010	/* used to be O_SYNC, see below */
+#endif
+#ifndef O_NONBLOCK
+#define O_NONBLOCK	0x0080
+#endif
+#ifndef O_CREAT
+#define O_CREAT		0x0100	/* not fcntl */
+#endif
+#ifdef O_TRUNC
+#define O_TRUNC		0x0200	/* not fcntl */
+#endif
+#ifndef O_EXCL
+#define O_EXCL		0x0400	/* not fcntl */
+#endif
+#ifndef O_NOCTTY
+#define O_NOCTTY	0x0800	/* not fcntl */
+#endif
+#ifndef FASYNC
+#define FASYNC		0x1000	/* fcntl, for BSD compatibility */
+#endif
+#ifndef O_LARGEFILE
+#define O_LARGEFILE	0x2000	/* allow large file opens */
+#endif
 /*
  * Before Linux 2.6.33 only O_DSYNC semantics were implemented, but using
  * the O_SYNC flag.  We continue to use the existing numerical value
@@ -30,14 +47,26 @@ https://github.com/torvalds/linux/blob/d358e5254674b70f34c847715ca509e46eb81e6f/
  *
  * Note: __O_SYNC must never be used directly.
  */
-// #define __O_SYNC	0x4000
-// #define O_SYNC		(__O_SYNC|O_DSYNC)
-// #define O_DIRECT	0x8000	/* direct disk access hint */
-
-// #define F_GETLK		14
-// #define F_SETLK		6
-// #define F_SETLKW	7
-
-// #define F_SETOWN	24	/*  for sockets. */
-// #define F_GETOWN	23	/*  for sockets. */
+#ifndef O_SYNC
+#define __O_SYNC	0x4000
+#define O_SYNC		(__O_SYNC|O_DSYNC)
+#endif /*O_SYNC*/
+#ifndef O_DIRECT
+#define O_DIRECT	0x8000	/* direct disk access hint */
 #endif
+#ifndef F_GETLK
+#define F_GETLK		14
+#endif
+#ifndef F_SETLK
+#define F_SETLK		6
+#endif
+#ifndef F_SETLKW
+#define F_SETLKW	7
+#endif
+#ifndef F_SETOWN
+#define F_SETOWN	24	/*  for sockets. */
+#endif
+#ifndef F_GETOWN
+#define F_GETOWN	23	/*  for sockets. */
+#endif
+#endif /*_SYS_STAT_H */
