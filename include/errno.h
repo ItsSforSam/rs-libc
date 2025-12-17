3c1e7107ff9322656dd5875a3e130a98bf9c04bf
SforSam <81501696+ItsSforSam@users.noreply.github.com>
2025-12-17 12:48:11 -0600

    moved errno macro to appropriate header

--- include/errno.h
+++ include/errno.h
@@ -3,5 +3,15 @@
 #define _ERRNO_H
 
 #include <sys/errno.h>
+/*
+  We generate consts and non-libc functions to use in rust.
+  To avoid having functions and the like to be attempted to be
+  linked found with sys crate when it doesn't exit 
+*/
+#ifndef __RSLIBC_BUILD_BOOTSTRAP
 
+    int* __get_errno_ptr();
+    #define errno *__get_errno_ptr()
+
+#endif /*__RSLIBC_BUILD_BOOTSTRAP */
 #endif
--- include/unistd.h
+++ include/unistd.h
@@ -1,15 +1,5 @@
 #ifndef _UNISTD_H
 #define _UNISTD_H
 
-/*
-  We generate consts and non-libc functions to use in rust.
-  To avoid having functions and the like to be attempted to be
-  linked found with sys crate when it doesn't exit 
-*/
-#ifndef __RSLIBC_BUILD_BOOTSTRAP
 
-    int* __get_errno_ptr();
-    #define errno *__get_errno_ptr()
-
-#endif /*__RSLIBC_BUILD_BOOTSTRAP */
 #endif /* _H_UNISTD */
