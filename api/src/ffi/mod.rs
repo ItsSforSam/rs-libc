9537e3f3884d4add36625bb7c686f08e38b4bc01
SforSam <81501696+ItsSforSam@users.noreply.github.com>
2026-01-16 13:53:15 -0600

    Removed c_str.rs module, using alloc impl

--- api/src/ffi/mod.rs
+++ api/src/ffi/mod.rs
@@ -1,7 +1,12 @@
 //! FFI which contains abstractions for the underlying OS
 //! 
 //! Similar to the [`std::ffi`]
-//! 
+#![expect(ambiguous_glob_reexports, reason = "We will be defining our own module")]
 pub use core::ffi::*;
 #[cfg(feature="alloc")]
-mod c_str;
+pub use alloc::ffi::{CString,FromVecWithNulError,IntoStringError,NulError};
+
+/// [`CStr`],[`CString`], and relating types
+pub mod c_str{
+    
+}
