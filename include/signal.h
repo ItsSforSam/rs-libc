079a5701ceef360df8ec2def277d2be8d659bf62
SforSam <81501696+ItsSforSam@users.noreply.github.com>
2026-02-02 14:48:23 -0600

    Reexport <asm/signal.h> in include files
    This should be redone to avoid polluting namespaces

--- include/signal.h
+++ include/signal.h
@@ -1,7 +1,6 @@
 #ifndef _H_SIGNAL
 #define _H_SIGNAL
-
-// @TODO: implment all the signals
-#define SIGABRT 6
+// @TODO: Port it over to avoid polluting namespaces
+#include <asm/signal.h>
 
 #endif /* _H_SIGNAL */
