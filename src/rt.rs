fdcb4004c1fa02b0e13930e7f2d8c8d017e27a23
SforSam <ItsSforSam@users.noreply.github.com.>
2026-02-13 14:00:02 -0600

    Have the main function actually run

--- src/rt.rs
+++ src/rt.rs
@@ -4,6 +4,7 @@
 
 
 
+
 type MainFn = extern "C" fn(argc:core::ffi::c_int, argv: *mut *mut core::ffi::c_char)-> core::ffi::c_int;
 /// Entrypoint from crt0. Should not be 
 /// 
@@ -16,6 +17,7 @@ unsafe extern "C" fn __rslibc_start_entrypoint_1(
     argv:*mut *mut core::ffi::c_char,
     argc:core::ffi::c_int
 )->!{
-    todo!()
+    // @TODO: initialize the c runtime
+    api::quick_exit(mainfn(argc,argv))
 }
 
