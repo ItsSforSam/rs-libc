563913709d5c5fd975f0563561809c8ff373922f
SforSam <ItsSforSam@users.noreply.github.com.>
2026-02-13 14:38:15 -0600

    Have compile-test.sh report failure

--- tests/compile-test.sh
+++ tests/compile-test.sh
@@ -2,7 +2,13 @@
 
 # out=$(mktemp)
 out=./test.elf
-clang -nostartfiles  -nodefaultlibs -nostdlib ./tests/compile-test.c ./target/debug/librs_libc.so ./target/debug/libcrt0.a -isystem ./include -o "$out"
+clang -nostartfiles  -nodefaultlibs -nostdlib ./tests/compile-test.c ./target/debug/librs_libc.so ./target/debug/libcrt0.a -isystem ./include -o "$out" 
+if [[ "$?" -eq "0" ]]; then
+    echo "Compile Successful!"
+else
+    echo "Something went wrong!" >&2
+    exit -1
+fi
 "$out"
-echo $?
+echo "Test program exited with `$?`"
 # rm -r $out
