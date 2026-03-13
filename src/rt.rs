11805b284c5e4bc0667c0c451f2c2e45b80f86b9
SforSam <ItsSforSam@users.noreply.github.com>
2026-03-13 14:24:30 -0500

    Add global Libc instance
    This just holds the state of the whole program

--- src/rt.rs
+++ src/rt.rs
@@ -1,8 +1,23 @@
 //! Runtime routines for the runtime.
 //! 
 //! <b>These are internal details and should not be relied upon</b>
+use rslibc_common::Libc;
+pub static LIBC: Libc = Libc::default();
 
 
+
+
+/// This is aliased to C as the errno with
+/// 
+/// ```C
+/// #define errno {*__get_errno_ptr()}
+/// ```
+// SAFETY: double underscore prefix makes it impl specific and there shouldn't be and other libc loaded 
+#[unsafe(no_mangle)]
+#[linkage = "weak"] // TODO: find something better
+extern "C" fn __get_errno_ptr() -> *mut core::ffi::c_int{
+    LIBC.get_errno_raw()
+}
 
 
 type MainFn = extern "C" fn(argc:core::ffi::c_int, argv: *mut *mut core::ffi::c_char)-> core::ffi::c_int;
@@ -18,7 +33,8 @@ unsafe extern "C-unwind" fn __rslibc_start_entrypoint_1(
     argc:core::ffi::c_int,
     envp:*mut *mut core::ffi::c_char
 )->!{
-    // @TODO: initialize the c runtime
+    // @TODO: initialize the c runtime, like the libc struct
+    // get the auxiliary vector if available
     api::quick_exit(mainfn(argc,argv))
 }
 
