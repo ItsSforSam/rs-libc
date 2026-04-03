71b458f580f520a3d261ae0b7f1f33a73cfeee6c
SforSam <ItsSforSam@users.noreply.github.com>
2026-04-03 13:28:05 -0500

    Add stat struct

--- include/sys/stat.h
+++ include/sys/stat.h
@@ -7,6 +7,7 @@ https://github.com/torvalds/linux/blob/d358e5254674b70f34c847715ca509e46eb81e6f/
 #ifndef _SYS_STAT_H
 #define _SYS_STAT_H
 #include <asm-generic/fcntl.h>
+#include <sys/types.h>
 #ifndef O_APPEND
     #define O_APPEND	0x0008
 #endif
@@ -69,4 +70,46 @@ https://github.com/torvalds/linux/blob/d358e5254674b70f34c847715ca509e46eb81e6f/
 #ifndef F_GETOWN
 #define F_GETOWN	23	/*  for sockets. */
 #endif
+
+typedef signed long blksize_t
+
+struct stat {
+    dev_t      st_dev;      /* ID of device containing file */
+    ino_t      st_ino;      /* Inode number */
+    mode_t     st_mode;     /* File type and mode */
+    nlink_t    st_nlink;    /* Number of hard links */
+    uid_t      st_uid;      /* User ID of owner */
+    gid_t      st_gid;      /* Group ID of owner */
+    dev_t      st_rdev;     /* Device ID (if special file) */
+    off_t      st_size;     /* Total size, in bytes */
+    blksize_t  st_blksize;  /* Block size for filesystem I/O */
+    blkcnt_t   st_blocks;   /* Number of 512 B blocks allocated */
+
+    /* Since POSIX.1-2008, this structure supports nanosecond
+        precision for the following timestamp fields.
+        For the details before POSIX.1-2008, see VERSIONS.  */
+
+    struct timespec  st_atim;  /* Time of last access */
+    struct timespec  st_mtim;  /* Time of last modification */
+    struct timespec  st_ctim;  /* Time of last status change */
+
+#define st_atime  st_atim.tv_sec  /* Backward compatibility */
+#define st_mtime  st_mtim.tv_sec
+#define st_ctime  st_ctim.tv_sec
+};
+struct stat64
+  {
+    mode_t st_mode;		/* File mode.  */
+    ino64_t st_ino;		/* File serial number.	*/
+    dev_t st_dev;		/* Device.  */
+    nlink_t st_nlink;		/* Link count.  */
+
+    uid_t st_uid;		/* User ID of the file's owner.	*/
+    gid_t st_gid;		/* Group ID of the file's group.*/
+    off64_t st_size;		/* Size of file, in bytes.  */
+
+    time_t st_atime;		/* Time of last access.  */
+    time_t st_mtime;		/* Time of last modification.  */
+    time_t st_ctime;		/* Time of last status change.  */
+  };
 #endif /*_SYS_STAT_H */
